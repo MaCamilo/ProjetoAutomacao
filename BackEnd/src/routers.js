afa156bc3189fa84f66a7d4dea6327f505ac01e2
Pedro <pedrohcardoso2104@gmail.com>
2024-12-04 14:48:58 -0300

    login feito

--- BackEnd/src/routers.js
+++ BackEnd/src/routers.js
@@ -7,36 +7,36 @@ const router = express.Router();
 
 
 
-router.post("/cadastrarCliente", async (req, res) => {
+// router.post("/cadastrarCliente", async (req, res) => {
 
-    const { id, nome, sobrenome, CPF, CEP, email, senha } = req.body
+//     const { id, nome, sobrenome, CPF, CEP, email, senha } = req.body
 
-    const password = await bcrypt.hash(senha, salt)
-    console.log(password);
+//     const password = await bcrypt.hash(senha, salt)
+//     console.log(password);
 
-    if (check.length > 0) {
-        res.json({ msg: "Verifique o seu Email ou CPF" })
-    }
-    else {
-        try {
+//     if (check.length > 0) {
+//         res.json({ msg: "Verifique o seu Email ou CPF" })
+//     }
+//     else {
+//         try {
 
-            const cadastrar = connection.query("INSERT INTO cliente values (?,?,?,?,?,?,?)", [id, nome, sobrenome, CPF, CEP, email, senha,], (erro) => {
-                if (erro) {
-                    res.status(400).json(erro)
-                }
+//             const cadastrar = connection.query("INSERT INTO cliente values (?,?,?,?,?,?,?)", [id, nome, sobrenome, CPF, CEP, email, senha,], (erro) => {
+//                 if (erro) {
+//                     res.status(400).json(erro)
+//                 }
 
-                else {
-                    res.status(200).json("Cadastrado com sucesso!!!");
-                }
+//                 else {
+//                     res.status(200).json("Cadastrado com sucesso!!!");
+//                 }
 
-            })
-        }
-        catch (erro) {
-            console.log(erro)
-        }
-    }
+//             })
+//         }
+//         catch (erro) {
+//             console.log(erro)
+//         }
+//     }
 
-})
+// })
 
 // router.post("/cadastrarEndereco", async (req, res) => {
 //     const { id, rua, cep, cidade, estado, bairro } = req.body
@@ -90,12 +90,12 @@ router.post('/api/contato', async(req, res)=>{
 router.post('/login',async(req,res)=>{
     const {email,senha} = req.body
 
-    const sql = "SELECT * FROM alunos WHERE email = ?"
+    const password = await bcrypt.hash(senha,salt)
+    console.log(password);
 
+    const sql = "SELECT * FROM cliente WHERE email = ?"
     try{
         const [rows] = await connection.promise().query(sql,[email]);
-
-        console.log(rows[0].sobrenome)
 
         if(rows.length > 0){
             const match = await bcrypt.compare(senha,rows[0].senha);
