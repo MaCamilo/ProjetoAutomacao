d223a19f10b937cceff9951069c3c92280fef7fe
Pedro <pedrohcardoso2104@gmail.com>
2024-12-04 11:16:09 -0300

    teste rodape cadastro

--- BackEnd/src/routers.js
+++ BackEnd/src/routers.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.post("/cadastrarCliente", async(req,res)=>{
         
-    const {id,nome,sobrenome,CPF,CPE,email,senha} = req.body
+    const {id,nome,sobrenome,CPF,CEP,email,senha} = req.body
 
     const sql = "SELECT * FROM cliente WHERE email = ?"
     const [check] = await connection.query(sql, [email])
@@ -20,13 +20,13 @@ router.post("/cadastrarCliente", async(req,res)=>{
     else{  
     try{
             
-        connection.query("INSERT INTO cliente values (?,?,?,?,?,?,?)",[id,nome,sobrenome,CPF,CPE,email,senha,], (erro)=>{
+        const cadastrar = connection.query("INSERT INTO cliente values (?,?,?,?,?,?,?)",[id,nome,sobrenome,CPF,CEP,email,senha,], (erro)=>{
             if(erro){
                 res.status(400).json(erro)
             }
 
             else{
-                res.status(200).json("Cadastrado com sucesso!!!");
+                res.status(201).json("Cadastrado com sucesso!!!");
             }
                 
         })
@@ -38,7 +38,7 @@ router.post("/cadastrarCliente", async(req,res)=>{
         
 })
 
-router.post("/cadastrarEndereco", (req,res)=>{
+router.post("/cadastrarEndereco", async(req,res)=>{
     const {id,rua,cep,cidade,estado,bairro} = req.body
 
     try{
@@ -59,10 +59,14 @@ router.post("/cadastrarEndereco", (req,res)=>{
 })
 
 
-router.post("/contato", (req,res)=>{
-    const {id,nome,sobrenome,CPF,CEP,email,senha,} = req.body
+router.post("/contato", async(req,res)=>{
+    const {id,nome,sobrenome,CPF,CEP,email,senha,} = req.body;
+    console.log(req.body);
+
+    const password = await bcrypt.hash(senha,salt)
+        console.log(password);
     try{
-        connection.query("INSERT INTO cliente(id,nome,sobrenome,CPF,CPE,email,senha) values (?,?,?,?,?,?,?)", [id,nome,sobrenome,CPF,CEP,email,senha,], (erro)=>{
+        connection.query("INSERT INTO cliente values (?,?,?,?,?,?,?)", [id,nome,sobrenome,CPF,CEP,email,senha,], (erro)=>{
             if(erro){
                 res.status(400).json(erro)
             }
